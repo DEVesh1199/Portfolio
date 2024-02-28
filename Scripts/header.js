aacacd5b9998f484762b446ddeb9bd67cfb29c15
Devesh Kumar <kumar.devesh1818@gmail.com>
2024-02-28 15:46:25 +0530

    added responsive header to portfolio website

---
+++ Scripts/header.js
@@ -0,0 +1,11 @@
+flag = 0;
+document.querySelector('.js-hamburger').addEventListener('click', ()=>{
+  if(flag === 0){
+    document.getElementById('js-sidenav').style.transform = 'translateX(0px)';
+    flag = 1;
+  } else {
+    document.getElementById('js-sidenav').style.transform = 'translateX(150px)';
+    flag = 0;
+  }
+  
+});
